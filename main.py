8cd2b5ae7bbbb954f99097a674ce3796f5040828
120864 <michaelpschreiber@gmail.com>
2024-05-17 13:16:37 -0700

    added main() method like you would in java

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("hello world")
+import random
+import trickycounter
+
+
+def hello_world(text: str) -> None:
+    print(text)
+
+
+def main() -> None:
+    # trickycounter.tricky_counter_method()
+    hello_world("this is just a cool new thing")
+
+
+if __name__ == '__main__':
+    main()
+
+# #
+# # for i in {1, 2, 3, 4, 5}:
+# #     a = i + 5
+# #     if a > 7:
+# #         print(f'hello friend ')
+# #     else:
+# #         print(a)
+# #
+#
+#
+# # Define the prefix for the keys
+# prefix = 'a'
+#
+# # Define an empty dictionary to hold the results
+# ip_dict = {}
+#
+# # Generate 11 keys with random IP addresses
+# for i in range(11):
+#     key = prefix + str(i)
+#     ip = '.'.join([str(random.randint(0, 255)) for _ in range(4)])
+#     ip_dict[key] = ip
+#
+# ## to print the key value pair as a tuple
+# for i in ip_dict.items():
+#     print(i)
+# ## OR... if you just want to print the string value of the key and the value held for each key
+# for key, value in ip_dict.items():
+#     print(key, value)
+# ## to print just the values of the dictionary
+# # for i in ip_dict.values():
+# #     print(i)
+#
---
+++ trickycounter.py
@@ -0,0 +1,12 @@
+def tricky_counter_method():
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    counter = 0
+
+    for i in my_list:
+        counter = counter + i
+
+    print(counter)
+
+
+
