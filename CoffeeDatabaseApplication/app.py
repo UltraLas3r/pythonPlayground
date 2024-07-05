acf12de5b14c2b6004addff83648041e1d888062
120864 <michaelpschreiber@gmail.com>
2024-07-05 08:57:26 -0700

    coffee database application

--- CoffeeDatabaseApplication/app.py
+++ CoffeeDatabaseApplication/app.py
@@ -1,6 +1,68 @@
 from CoffeeDatabaseApplication import database
 
+MENU_PROMPT = """~~~!!~-- Coffee Bean Application --~!!~~~
+
+Please Choose from the following options:
+
+1) Add a new bean.
+2) See all beans.
+3) Find a bean by name.
+4) See best preparation methods for a bean.
+5) Exit.
+
+Enter your selection number: 
+"""
+
 
 def menu():
-    connection: database.connect()
+    connection = database.connect()
     database.create_tables(connection)
+
+    while (user_input := input(MENU_PROMPT)) != "5":
+        if user_input == "1":
+            prompt_add_new_bean(connection)
+
+        elif user_input == "2":
+            prompt_see_all_beans(connection)
+
+        elif user_input == "3":
+            prompt_find_bean_by_name(connection)
+
+        elif user_input == "4":
+            prompt_see_best_preparation_methods_for_a_bean(connection)
+
+        else:
+            print("Invalid input, please try again!")
+
+
+def prompt_add_new_bean(connection):
+    name = input("Enter bean name: ")
+    method = input("Enter how you prepared your coffee: ")
+    rating = int(input("Enter your rating score: "))
+
+    database.add_bean(connection, name, method, rating)
+
+
+def prompt_see_all_beans(connection):
+    beans = database.get_all_beans(connection)
+
+    for bean in beans:
+        print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+
+
+def prompt_find_bean_by_name(connection):
+    name = input("Enter bean name to find info: ")
+    beans = database.get_beans_by_name(connection, name)
+
+    for bean in beans:
+        print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+
+
+def prompt_see_best_preparation_methods_for_a_bean(connection):
+    name = input("Enter bean name for preparation information: ")
+    method = database.get_best_preparation_for_bean(connection, name)
+
+    print(f"The best preparation method for {name} is the {method[2]} method.")
+
+
+menu()
--- CoffeeDatabaseApplication/database.py
+++ CoffeeDatabaseApplication/database.py
@@ -1,9 +1,14 @@
 import sqlite3
 
-CREATE_BEANS_TABLE = "CREATE TABLE beans (id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER"
+CREATE_BEANS_TABLE = "CREATE TABLE IF NOT EXISTS beans (id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER)"
 INSERT_BEAN = "INSERT INTO beans (name, method, rating) VALUES (?, ?, ?);"
 GET_ALL_BEANS = "SELECT * FROM beans;"
 GET_BEANS_BY_NAME = "SELECT * FROM beans WHERE name = ?;"
+GET_BEST_PREPARATION_METHOD_FOR_BEAN = """
+                                        SELECT * FROM beans 
+                                        WHERE name = ?
+                                        ORDER BY rating DESC
+                                        LIMIT 1;"""
 
 
 def connect():
@@ -24,9 +29,14 @@ def add_bean(connection, name, method, rating):
 
 def get_all_beans(connection):
     with connection:
-        connection.execute(GET_ALL_BEANS)
+        return connection.execute(GET_ALL_BEANS).fetchall()
 
 
 def get_beans_by_name(connection, name):
     with connection:
-        connection.execute(GET_BEANS_BY_NAME, name)
+        return connection.execute(GET_BEANS_BY_NAME, (name,)).fetchall()
+
+
+def get_best_preparation_for_bean(connection, name):
+    with connection:
+        return connection.execute(GET_BEST_PREPARATION_METHOD_FOR_BEAN, (name,)).fetchone()
